251396b65379c836119c096981d5d976e78c9da2
Renat Osmanov <rhenium.osmium@gmail.com>
2016-08-10 00:39:52 +0300

    check redux work

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,6 +1,13 @@
 import React, {PropTypes, Component} from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { createMessage } from '../../redux/modules/message';
 
 class App extends Component {
+  componentWillMount() {
+    this.props.dispatch(createMessage('hello'));
+  }
+  
   render() {
     return (
       <h1>HELLO how are you????</h1>
@@ -8,4 +15,12 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapStateToProps({message}) {
+  return {
+    content:  message
+  }
+}
+
+export default connect(mapStateToProps)(App);
+
+//export default App;
--- src/redux/modules/message.js
+++ src/redux/modules/message.js
@@ -2,17 +2,17 @@ const CREATE_MESSAGE='react-forum/message/CREATE_MESSAGE';
 
 const initialState = {isFetching:false};
 
-export function create(message){
+export function createMessage(content){
   return {
     type: CREATE_MESSAGE,
-    message
+    content
   }
 }
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case CREATE_MESSAGE:
-      return Object.assign({},action.message);
+      return Object.assign({},state,{content:action.content});
     default:
       return state;
   }
